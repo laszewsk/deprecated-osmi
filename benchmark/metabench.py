0c4d51707514e713be304cb7bff342bd8e552ba6
Nate Kimball <tma5gv@virginia.edu>
2023-02-17 14:28:34 -0500

    start of new repo

---
+++ benchmark/metabench.py
@@ -0,0 +1,123 @@
+"""
+Example usage:
+
+Start servers on multiple GPUs:
+
+    ./1_start_tfs_servers.sh
+
+Then run this code e.g. to use 2 GPUs with a sweep of concurrencies 1 2 4 8 16 per GPU:
+
+    export CUDA_VISABLE_DEVICES=0,1
+    python metabench.py localhost -g T4 -b 64 -c 1 2 4 8 16 --ports 8500 8501 8502 8503 -n 2
+
+or, can use a YAML file to specify all the options, e.g.:
+
+    python metabench.py rivanna-v100.yaml -o results-v100.csv
+
+"""
+
+import argparse
+import csv
+import datetime
+import glob
+import os
+import re
+import subprocess
+import yaml
+from cloudmesh.common.StopWatch import StopWatch
+from cloudmesh.common.Shell import Shell
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Where to store intermediate files
+out_path = "."
+#out_path = os.environ['WORKDIR']
+
+# Grab the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('server', help='server ip:port, e.g. 10.1.1.37:8080')
+parser.add_argument('-g', '--gpu', type=str, default="none", help='enter in the GPU type {V100, P100}')
+parser.add_argument('-b', '--batch', nargs='+', type=int)
+parser.add_argument('-n', '--nrequests', nargs='+',type=int)
+parser.add_argument('-c', '--concurrency', nargs='+', type=int)
+parser.add_argument('--ngpus', nargs='+', type=int)
+parser.add_argument('-p', '--ports',  nargs='+', type=int)
+parser.add_argument('-o', '--outfn', default='results.csv')
+parser.add_argument('-m', '--model', nargs='+', type=str)
+parser.add_argument('-a', '--algorithm', default='tfs_grpc_client.py')
+parser.add_argument('-y', '--config', type=str)
+args = parser.parse_args()
+
+with open (args.outfn, 'w') as csvfile:
+    cw = csv.writer(csvfile, delimiter=',')
+    cw.writerow("prg:Timestamp:GPU:Server:Concurrency:Model:# of Requests:BatchSize:Throughput (inf/s):Latency (ms)".split(":"))
+
+extract = lambda x: float(re.findall('\d+.\d+', x)[0])
+
+if ".yaml" in args.server:
+    # read and parse YAML file
+    with open(args.server,'r') as f:
+        sections = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+    for section, options in sections.items():
+        for option, value in options.items():
+            setattr(args, option, value)
+
+print(args)
+
+Shell.mkdir("results")
+StopWatch.start("loop")
+
+for model in args.model: # e.g. ["small_lstm","medium_cnn","large_tcnn"]
+    print(f"\nmodel: {model}")
+    for nrequests in args.nrequests: # e.g. [50,100]
+        print(f"nrequests: {nrequests}")
+        for batchsize in args.batch: # e.g. [16, 64]
+            print(f"batchsize: {batchsize}")
+            StopWatch.start(f"batchsize-{batchsize}")
+            for ngpus in args.ngpus: # e.g. [1, 2, 4, 8]
+                print(f"ngpus: {ngpus}")
+                for concurrency in args.concurrency: # e.g. [1, 2, 4, 8, 16]
+                    print(f"concurrency: {concurrency}")
+                    proc = []
+                    # filenames = []
+                    for server_id, port in enumerate(args.ports[:ngpus], start=1): #[8500....8505]
+                        for client_rank in range(concurrency):
+                            cmd = f"python {args.algorithm} {args.server}:{port} -m {model} -b {batchsize} -n {nrequests}"
+                            log_file = f"results/log-{model}-{nrequests}-{batchsize}-{ngpus}-{concurrency}-{server_id}-{port}-{client_rank}.txt"
+                            # cmd = f"python {args.algorithm} {args.server}:{port} -m {model} -b {batchsize} -n {nrequests}" 
+                            print(log_file)
+                            timestamp = datetime.datetime.now().time()
+                            print(timestamp, cmd)
+                            f = open(log_file,"w")
+                            proc.append(subprocess.Popen(cmd, stdout=f, stderr=subprocess.STDOUT, shell=True))
+                            # filenames.append(f"log{server_id}.{client_rank}.txt")
+
+                    # Barrier 
+                    exit_codes = [p.wait() for p in proc]
+
+                    filenames = glob.glob(os.path.join(out_path, "results/log*"))
+                    print(filenames)
+                    num_files = len(filenames)
+                    throughput = latency = 0
+
+                    for fn in filenames:
+                        with open(fn) as f:
+                            lines = f.readlines()
+
+                        for line in lines:
+                            if re.search("throughput", line): throughput += extract(line)
+                            if re.search("latency", line): latency += extract(line)
+                        
+                        # Delete log files after getting perf metrics.
+                        os.unlink(fn)
+                    
+                    avg_latency = latency/num_files
+                    print(f"throughput: {throughput:.2f}") 
+                    print(f"avg latency: {avg_latency:.2f}")
+                    with open(args.outfn, 'a') as csvfile:
+                        cw = csv.writer(csvfile, delimiter=',')
+                        cw.writerow([timestamp, args.gpu, server_id, concurrency, model, nrequests, batchsize, throughput, avg_latency])
+            StopWatch.stop(f"batchsize-{batchsize}")
+StopWatch.stop("loop")
+StopWatch.benchmark()
