66fde710e981f6c7d18c688e66abbcf8786e8b10
Gregor von Laszewski <laszewski@gmail.com>
2023-07-25 22:49:39 -0400

    add model server example template

---
+++ target/rivanna/ModelServer.py
@@ -0,0 +1,61 @@
+class ModelServer:
+
+    def __int__(self, port=None, exec_dir=None):
+        raise NotImplementedError
+        self.visible_devices = os.environ("CUDA_VISIBLE_DEVICES")
+        self.port = port
+
+    def start(self):
+        command = f" time {self.visible_devices}=$j singularity exec --nv --home `pwd` $EXEC_DIR/serving_latest-gpu.sif"\
+                  f" tensorflow_model_server --port={self.port} --rest_api_port=0 --model_config_file=models.conf >& $OUTPUT_DIR/v100-$PORT.log &
+    done"
+        Shell.run (command)
+        raise NotImplementedError
+
+    def shutdown(self):
+        raise NotImplementedError
+
+    def status(self):
+        # ok
+        # failed
+        raise NotImplementedError
+
+"""
+    
+start_tf_servers() {
+    for j in `seq 0 $(($NUM_GPUS_PER_NODE-1))`
+    do
+        PORT=$(($PORT_TF_SERVER+$j))
+        time CUDA_VISIBLE_DEVICES=$j singularity exec --nv --home `pwd` $EXEC_DIR/serving_latest-gpu.sif tensorflow_model_server --port=$PORT --rest_api_port=0 --model_config_file=models.conf >& $OUTPUT_DIR/v100-$PORT.log &
+    done
+
+    # time CUDA_VISIBLE_DEVICES=1 singularity exec --nv --home `pwd` $EXEC_DIR/serving_latest-gpu.sif tensorflow_model_server --port=8501 --rest_api_port=0 --model_config_file=models.conf >& $OUTPUT_DIR/v100-8501.log &
+    echo "# cloudmesh status=running progress=45 pid=$$"
+    start_wait_for_server=`date +%s`
+    echo start_wait_for_server $start_wait_for_server
+
+    # for sec in $(seq -w 10000 -1 1); do
+    #     if [[ $(lsof -i :8500) && $(lsof -i :8501) ]]; then break; fi
+    #     sleep 0.5
+    #     echo "-"
+    # done
+    for sec in $(seq -w 10000 -1 1); do
+        valid=1
+        for PORT in `seq $PORT_TF_SERVER $(($PORT_TF_SERVER+$NUM_GPUS_PER_NODE-1))`; do
+            if ! [[ $(lsof -i :$PORT) ]]; then
+                valid=0;
+                break;
+            fi
+        done
+        if [ $valid = 1 ]; then break; fi;
+        sleep 0.5
+        echo "-"
+    done
+
+    end_wait_for_server=`date +%s`
+    echo "server up"
+    echo end_wait_for_server $end_wait_for_server
+    time_server_wait=$((end_wait_for_server-start_wait_for_server))
+    echo time_server_wait $time_server_wait
+}
+    """
