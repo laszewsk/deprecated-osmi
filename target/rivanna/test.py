e2f9cc16e3dbd86611030cdb09e2649a2f5c46e3
Nate Kimball <tma5gv@virginia.edu>
2023-08-07 20:20:02 -0400

    synchronizing

--- target/rivanna/test.py
+++ target/rivanna/test.py
@@ -8,8 +8,8 @@ config = FlatDict()
 config.load("osmi.in.yaml", expand=True)
 
 pprint (dict(config))
-e = expand_config_parameters(flat=config, expand_yaml=True, expand_os=True, expand_cloudmesh=True)
+expand_config_parameters(flat=config, expand_yaml=True, expand_os=True, expand_cloudmesh=True)
 
 print ("TTTT")
-pprint (dict(e))
+pprint (dict(config))
 
