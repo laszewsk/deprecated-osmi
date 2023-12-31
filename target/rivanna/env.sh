3224507cf780765af794c1bb803a2761dbd21339
Gregor von Laszewski <laszewski@gmail.com>
2023-12-31 12:05:09 -0500

    add emv.sh script

---
+++ target/rivanna/env.sh
@@ -0,0 +1,34 @@
+export USER_SCRATCH=/scratch/$USER
+export USER_LOCALSCRATCH=/localscratch/$USER
+
+export CLOUDMESH_CONFIG_DIR=$USER_SCRATCH/.cloudmesh
+export APPTAINER_CACHEDIR=$USER_SCRATCH/.apptainer/cache
+
+export OSMI_PROJECT=$USER_SCRATCH/osmi
+export OSMI_TARGET=$OSMI_PROJECT/target/rivanna
+
+mkdir -p $APPTAINER_CACHEDIR
+
+echo "============================================================="
+echo "USER_SCRATCH:        " $USER_SCRATCH
+echo "USER_LOCALSCRATCH:   " $USER_LOCALSCRATCH
+echo "CLOUDMESH_CONFIG_DIR:" $CLOUDMESH_CONFIG_DIR
+echo "APPTAINER_CACHEDIR:  " $APPTAINER_CACHEDIR
+echo "OSMI_PROJECT:        " $OSMI_PROJECT
+echo "OSMI_TARGET:         " $OSMI_TARGET
+echo "============================================================="
+
+echo "Load apptainer ..."
+module load apptainer
+
+echo "Create Python OSMI environment ..."
+module load gcc/11.4.0  openmpi/4.1.4 python/3.11.4
+python -m venv $USER_SCRATCH/OSMI # takes about 5.2s
+source $USER_SCRATCH/OSMI/bin/activate
+pip install --upgrade pip > /dev/null
+
+echo "============================================================="
+echo "Python version: " $(python --version)
+echo "Python path:    " $(which python)
+echo "Pip version:    " $(pip --version)
+echo "============================================================="
